109dcb767105ec2d5f1a763a7d5c8e6a4e68a51b
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-10-20 23:19:16 +0300

    Modified MySQL Error

--- Intro_to_DB/MySQLServer.py
+++ Intro_to_DB/MySQLServer.py
@@ -1,24 +1,23 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
-        # Connect to MySQL server
+        # Connect to the MySQL server
         connection = mysql.connector.connect(
             host='localhost',
-            user='your_username',  #  MySQL username
-            password='your_password'  # MySQL password
+            user='your_username',  # MySQL username
+            password='your_password'  #  MySQL password
         )
 
         if connection.is_connected():
             cursor = connection.cursor()
 
-            # Create the database if it doesn't already exist
+            # Create the database if it doesn't exist
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except mysql.connector.Error as e:  # handles MySQL errors
-        print(f"MySQL Error: {e}")
+    except mysql.connector.Error:  # Error line 
+        print("A MySQL error occurred.")
 
     finally:
         # Ensure resources are released properly
